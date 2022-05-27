51aa1ba852c60cc2aa2fc26e6e04c0c8a1177d8a
andrea_spinnicchia <104068000+andreaspinnicchia@users.noreply.github.com>
2022-05-27 08:56:38 +0200

    example of database creation

---
+++ google_certificate_data/sql/schools.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE schools;
+
+USE schools;
+
+CREATE TABLE district_2020 (
+  id integer PRIMARY KEY,
+    school_2020 text
+);
+
+CREATE TABLE district_2035 (
+   id integer PRIMARY KEY,
+    school_2035 text
+);
+
+INSERT INTO district_2020 VALUES
+    (1, 'Oak Street School'),
+    (2, 'Roosevelt High School'),
+    (5, 'Dover Middle School'),
+    (6, 'Webutuck High School');
+
+INSERT INTO district_2035 VALUES
+    (1, 'Oak Street School'),
+    (2, 'Roosevelt High School'),
+    (3, 'Morrison Elementary'),
+    (4, 'Chase Magnet Academy'),
+    (6, 'Webutuck High School');
+
